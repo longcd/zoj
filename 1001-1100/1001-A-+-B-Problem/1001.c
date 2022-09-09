f7b880a76b89f9e991d87e53926fb3bc97d45a81
longcd <longchangda@qq.com>
2022-09-09 09:56:31 +0800

    first commit

---
+++ 1001-1100/1001-A-+-B-Problem/1001.c
@@ -0,0 +1,17 @@
+// Source : https://zoj.pintia.cn/problem-sets/91827364500/problems/91827364500
+// Author : longcd
+// Time   : 20220909
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    while(scanf("%d %d", &a, &b) != EOF)
+    {
+        printf("%d\n", a + b);
+    }
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# My solutions to ZOJ
+
+I will put my solutions to [ZOJ](https://zoj.pintia.cn/problem-sets/91827364500/problems) in this repo.
